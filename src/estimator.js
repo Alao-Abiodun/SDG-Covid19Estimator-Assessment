cf25b269b3b63fe7fe5415885d30072574251293
Alao-Abiodun <abiodunDev@gmail.com>
2020-04-11 10:43:36 +0100

    solving the challenge 1-3 assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,107 @@
+const NofInfectedPeople = (currentlyInfected, periodType, timeToElapse) => {
+  if (periodType === 'months') {
+    // eslint-disable-next-line no-param-reassign
+    timeToElapse *= 30;
+  } else if (periodType === 'weeks') {
+    // eslint-disable-next-line no-param-reassign
+    timeToElapse *= 7;
+  }
+  const doubleFactors = 2 ** Math.floor(timeToElapse / 3);
+  return currentlyInfected * doubleFactors;
+};
+
+const impact = ({
+  reportedCases,
+  periodType,
+  timeToElapse,
+  totalHospitalBeds,
+  region: { avgDailyIncomeInUSD, avgDailyIncomePopulation }
+}) => {
+  const infected = reportedCases * 10;
+  const infectionsByRequestedTime = NofInfectedPeople(
+    infected,
+    periodType,
+    timeToElapse
+  );
+  const severeCasesByRequestedTime = Math.trunc(
+    0.15 * infectionsByRequestedTime
+  );
+  const hospitalBedsByRequestedTime = Math.trunc(
+    totalHospitalBeds * 0.35 - severeCasesByRequestedTime
+  );
+  const casesForICUByRequestedTime = Math.trunc(
+    0.05 * infectionsByRequestedTime
+  );
+  const casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * infectionsByRequestedTime
+  );
+
+  const dollarsInFlight = Math.trunc(
+    (infectionsByRequestedTime *
+      avgDailyIncomeInUSD *
+      avgDailyIncomePopulation) /
+      30
+  );
+
+  return {
+    currentlyInfected: infected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
+const severeImpact = ({
+  reportedCases,
+  periodType,
+  timeToElapse,
+  totalHospitalBeds,
+  region: { avgDailyIncomeInUSD, avgDailyIncomePopulation }
+}) => {
+  const infected = reportedCases * 50;
+  const infectionsByRequestedTime = NofInfectedPeople(
+    infected,
+    periodType,
+    timeToElapse
+  );
+  const severeCasesByRequestedTime = Math.trunc(
+    0.15 * infectionsByRequestedTime
+  );
+  const hospitalBedsByRequestedTime = Math.trunc(
+    totalHospitalBeds * 0.35 - severeCasesByRequestedTime
+  );
+  const casesForICUByRequestedTime = Math.trunc(
+    0.05 * infectionsByRequestedTime
+  );
+  const casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * infectionsByRequestedTime
+  );
+
+  const dollarsInFlight = Math.trunc(
+    (infectionsByRequestedTime *
+      avgDailyIncomeInUSD *
+      avgDailyIncomePopulation) /
+      30
+  );
+
+  return {
+    currentlyInfected: infected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
+
 const covid19ImpactEstimator = (data) => ({
-  data
+  data,
+  impact: impact(data),
+  severeImpact: severeImpact(data)
 });
 
 export default covid19ImpactEstimator;
