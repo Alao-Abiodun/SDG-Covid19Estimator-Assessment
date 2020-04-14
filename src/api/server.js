ccb0e7fa409aaae82ce321a5c2ce3d7faafc3e78
Alao-Abiodun <abiodunDev@gmail.com>
2020-04-14 23:37:40 +0100

    change the functionalities

--- src/api/server.js
+++ src/api/server.js
@@ -1,35 +1,36 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import xmlParser from 'js2xmlparser';
 import fs from 'fs';
 import path from 'path';
-import xml from 'xml';
-// const xml2js = require('xml2js');
 
 import covid19ImpactEstimator from '../estimator';
+// const xml2js = require('xml2js');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logged.txt'), {
+  flags: 'a'
+});
+
 
 const app = express();
 
 app.use(morgan('tiny'));
 
-const loggedStream = fs.createWriteStream(
-  path.join(__dirname, 'access.txt'),
-  { flags: 'a', encoding: 'utf8' }
-);
 
-morgan.token('response', (req, res) => {
-  // eslint-disable-next-line no-underscore-dangle
-  if (!res._header || !req._startAt) return '';
-  // eslint-disable-next-line no-underscore-dangle
-  const diff = process.hrtime(req._startAt);
-  let ms = diff[0] * 1e3 + diff[1] * 1e-6;
-  ms = ms.toFixed(0);
-  return `${ms.toString().padStart(2, '0')}ms`;
-});
+// morgan.token('response', (req, res) => {
+//   // eslint-disable-next-line no-underscore-dangle
+//   if (!res._header || !req._startAt) return '';
+//   // eslint-disable-next-line no-underscore-dangle
+//   const diff = process.hrtime(req._startAt);
+//   let ms = diff[0] * 1e3 + diff[1] * 1e-6;
+//   ms = ms.toFixed(0);
+//   return `${ms.toString().padStart(2, '0')}ms`;
+// });
 
 app.use(
-  morgan(':method :url :status :response\n', {
-    stream: loggedStream
+  morgan(':method # :url # :status # :response-time # ms', {
+    stream: accessLogStream
   })
 );
 
@@ -39,55 +40,14 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 4000;
 
-app.get('/api/v1/on-covid-19/logs', (req, res, next) => {
-  const dataInput = fs.readFileSync(path.join(__dirname, './logged.txt'), { encoding: 'utf-8' });
-  res.format({
-    // eslint-disable-next-line func-names
-    'text/plain': function () {
-      res.status(200).send(dataInput);
-    }
-  });
-  next();
-});
-
 app.post('/api/v1/on-covid-19/', (req, res, next) => {
-  const {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    }, periodType, timeToElapse, reportedCases, population, totalHospitalBeds
-  } = req.body;
-  const inputData = {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    },
-    periodType,
-    timeToElapse,
-    reportedCases,
-    population,
-    totalHospitalBeds
-  };
-  const estimateData = covid19ImpactEstimator(inputData);
-  res.status(200).send(estimateData);
+  const estimateData = covid19ImpactEstimator(req.body);
+  res.json(estimateData);
   next();
 });
 
 app.post('/api/v1/on-covid-19/json', (req, res, next) => {
-  const {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    }, periodType, timeToElapse, reportedCases, population, totalHospitalBeds
-  } = req.body;
-  const inputData = {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    },
-    periodType,
-    timeToElapse,
-    reportedCases,
-    population,
-    totalHospitalBeds
-  };
-  const estimateData = covid19ImpactEstimator(inputData);
+  const estimateData = covid19ImpactEstimator(req.body);
   res.status(200).json({
     estimateData
   });
@@ -95,26 +55,33 @@ app.post('/api/v1/on-covid-19/json', (req, res, next) => {
 });
 
 app.post('/api/v1/on-covid-19/xml', (req, res, next) => {
-  const {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    }, periodType, timeToElapse, reportedCases, population, totalHospitalBeds
-  } = req.body;
-  const inputData = {
-    region: {
-      name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation
-    },
-    periodType,
-    timeToElapse,
-    reportedCases,
-    population,
-    totalHospitalBeds
-  };
-  const estimateData = covid19ImpactEstimator(inputData);
+  let estimateData = covid19ImpactEstimator(req.body);
+  estimateData = xmlParser.parse('result', estimateData);
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(xml(estimateData));
+  res.status(200).send(estimateData);
   next();
 });
+
+app.post('/api/v1/on-covid-19/logs', (req, res, next) => {
+  fs.readFile('logged.txt', 'utf-8', (err, data) => {
+    let lines = data.split('\n');
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < lines.length - 1; i++) {
+      let line = lines[i].split(' # ');
+      line[3] = Math.round(line[3]);
+      line[3] = line[3] < 10 ? `0${line[3]}` : line[3];
+      const spaceOne = 7 - line[0].length;
+      const spaceTwo = 28 - line[1].length;
+      const spaceThree = 4;
+      line = `${line[0]}${' '.repeat(spaceOne)} ${line[1]}${' '.repeat(spaceTwo)} ${line[2]}${' '.repeat(spaceThree)} ${line[3]}${line[4]}`;
+      lines[i] = line;
+    }
+    lines = lines.join('\n');
+    res.type('text/plain').send(lines);
+  });
+  next();
+});
+
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
