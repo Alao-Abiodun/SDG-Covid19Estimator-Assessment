a666dd2f09b97d0f562fd61aaf40ba301b6bcefc
Alao-Abiodun <abiodunDev@gmail.com>
2020-04-14 22:04:16 +0100

    installing some libraries and working on challenge5 backend

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import xml from 'xml';
 // const xml2js = require('xml2js');
 
 import covid19ImpactEstimator from '../estimator';
@@ -9,11 +12,43 @@ const app = express();
 
 app.use(morgan('tiny'));
 
+const loggedStream = fs.createWriteStream(
+  path.join(__dirname, 'access.txt'),
+  { flags: 'a', encoding: 'utf8' }
+);
+
+morgan.token('response', (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  if (!res._header || !req._startAt) return '';
+  // eslint-disable-next-line no-underscore-dangle
+  const diff = process.hrtime(req._startAt);
+  let ms = diff[0] * 1e3 + diff[1] * 1e-6;
+  ms = ms.toFixed(0);
+  return `${ms.toString().padStart(2, '0')}ms`;
+});
+
+app.use(
+  morgan(':method :url :status :response\n', {
+    stream: loggedStream
+  })
+);
+
 app.use(bodyParser.json());
 
 // const builder = xml2js.Builder();
 
 const port = 4000 || process.env.PORT;
+
+app.get('/api/v1/on-covid-19/logs', (req, res, next) => {
+  const dataInput = fs.readFileSync(path.join(__dirname, './logged.txt'), { encoding: 'utf-8' });
+  res.format({
+    // eslint-disable-next-line func-names
+    'text/plain': function () {
+      res.status(200).send(dataInput);
+    }
+  });
+  next();
+});
 
 app.post('/api/v1/on-covid-19/', (req, res, next) => {
   const {
@@ -77,7 +112,7 @@ app.post('/api/v1/on-covid-19/xml', (req, res, next) => {
   };
   const estimateData = covid19ImpactEstimator(inputData);
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(estimateData);
+  res.status(200).send(xml(estimateData));
   next();
 });
 
